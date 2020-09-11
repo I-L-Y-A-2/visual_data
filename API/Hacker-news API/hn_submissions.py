014c506eb6a1766fcf16adc0e468804600d1c4a6
Ilya Verbilo <verbiloilyaa@gmail.com>
2020-09-11 14:50:08 +0300

    info from hn API

--- API/Hacker-news API/hn_submissions.py
+++ API/Hacker-news API/hn_submissions.py
@@ -1,5 +1,8 @@
 from operator import itemgetter
 import requests
+#import urllib3
+
+#urllib3.disable_warnings()
 
 # Создание вызова API и сохранение ответа.
 url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
@@ -12,9 +15,22 @@ submission_dicts = []
 for submission_id in submission_ids[:30]:
     # Создание отдельного вызова API для каждой статьи.
     url = f"https://hacker-new.firebasio.com/v0/item/{submission_id}.json"
-    r = requests.get(url)
+    r = requests.get(url, verify=False)
     print(f"id: {submission_id}\tstatus: {r.status_code}")
     response_dict = r.json()
 
     # Построение словаря для каждой статьи.
-    submission_dict = {}
+    submission_dict = {
+        'title': response_dict['title'],
+        'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
+        'comments': response_dict['descedants'],
+    }
+    submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
+                          reverse=True)
+
+for submission_dict in submission_dicts:
+    print(f"\nTitle: {submission_dict['title']}")
+    print(f"Discussion link: {submission_dict['hn_link']}")
+    print(f"Comments: {submission_dict['comments']}")
